f3bb9d7570f2b584b6fb48701a7c271b72f8e713
Elvi Kalinoski <elvi.kalinoski@gmail.com>
2024-03-06 17:16:34 -0300

    Update check-md5.sh

--- check-md5.sh
+++ check-md5.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 diretorio="/data"
-find "$diretorio" -type f -exec md5sum {} + > /tmp/checksums.txt
+find "$diretorio" -type f -exec md5sum {} + > checksums.txt
 echo "Checksums calculados e salvos em checksums.txt."
